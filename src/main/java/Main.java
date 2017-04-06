3403ddfb9ff10b7dad57d0fdf53c5de93d4b4cc5
DakMcMak <esbolov.galymzhan@mail.ru>
2017-04-06 22:37:10 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String s = "ssssssssss";
+        int i = 0;
+        while(i < 10){
+            System.out.println(s);
+            i++;
+        }
+    }
+}
